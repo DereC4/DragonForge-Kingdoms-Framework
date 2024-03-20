a1f374b64188f28c13fdd40a99126eb4e3961c8f
Derek Chen <derex1987@gmail.com>
2024-03-20 01:33:35 -0500

    Trying a new variant

--- src/main/java/io/github/derec4/dragonforgekingdoms/util/EncoderUtils.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/util/EncoderUtils.java
@@ -27,7 +27,10 @@ public class EncoderUtils {
     }
 
     public static int byteArrayToInt(byte[] bytes, int seek) {
-        return (bytes[seek] << 24) + (bytes[seek + 1] << 16) + (bytes[seek + 2] << 8) + bytes[seek + 3];
+        return ((bytes[seek] & 0xFF) << 24) |
+                ((bytes[seek + 1] & 0xFF) << 16) |
+                ((bytes[seek + 2] & 0xFF) << 8) |
+                (bytes[seek + 3] & 0xFF);
     }
 
     /*
