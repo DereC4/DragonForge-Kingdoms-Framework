537717c3829d7ab45927f69cd217ca5de29623dc
Derek Chen <derex1987@gmail.com>
2024-11-12 00:16:59 -0600

    feat: change owner admin command

--- src/main/java/io/github/derec4/dragonforgekingdoms/commands/AdminCommands.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/commands/AdminCommands.java
@@ -1,5 +1,6 @@
 package io.github.derec4.dragonforgekingdoms.commands;
 
+import io.github.derec4.dragonforgekingdoms.kingdom.Kingdom;
 import io.github.derec4.dragonforgekingdoms.kingdom.KingdomManager;
 import io.github.derec4.dragonforgekingdoms.database.CreateDB;
 import org.bukkit.Bukkit;
@@ -10,6 +11,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.UUID;
 
 import static io.github.derec4.dragonforgekingdoms.util.DatabaseUtils.removeKingdomFromDatabase;
@@ -53,6 +55,38 @@ public class AdminCommands implements CommandExecutor {
                     } catch (Exception e) {
                         Bukkit.getServer().getConsoleSender().sendMessage(e.toString());
                         sender.sendMessage(ChatColor.RED + "[ADMIN] An error occurred while deleting the kingdom.");
+                    }
+                }
+                case "changewner" -> {
+                    if (args.length < 3) {
+                        sender.sendMessage(ChatColor.RED + "Usage: /admin changeowner <kingdom name> <new owner>");
+                        return true;
+                    }
+                    String kingdomName = args[1];
+                    String newOwnerName = args[2];
+                    Player newOwner = Bukkit.getPlayer(newOwnerName);
+                    if (newOwner == null) {
+                        sender.sendMessage(ChatColor.RED + "Player " + newOwnerName + " not found or is offline.");
+                        return false;
+                    }
+                    UUID newOwnerUUID = newOwner.getUniqueId();
+                    CreateDB databaseManager = new CreateDB();
+                    try (Connection connection = databaseManager.getConnection()) {
+                        UUID kingdomUUID = km.getKingdomFromName(kingdomName);
+                        if (kingdomUUID != null) {
+                            Kingdom kingdom = km.getKingdomFromID(kingdomUUID);
+                            UUID oldOwnerUUID = kingdom.getLeader();
+                            kingdom.setLeader(newOwnerUUID);
+                            km.updatePlayerKingdom(connection, newOwnerUUID, kingdomUUID);
+                            km.removePlayerAsync(oldOwnerUUID);
+                            km.addPlayerToKingdom(newOwnerUUID, kingdomUUID);
+                            sender.sendMessage(ChatColor.GREEN + "[ADMIN] Kingdom " + kingdomName + " ownership has been transferred to " + newOwnerName + ".");
+                        } else {
+                            sender.sendMessage(ChatColor.RED + "[ADMIN] Kingdom " + kingdomName + " not found.");
+                        }
+                    } catch (SQLException e) {
+                        Bukkit.getServer().getConsoleSender().sendMessage(e.toString());
+                        sender.sendMessage(ChatColor.RED + "[ADMIN] An error occurred while changing the kingdom owner.");
                     }
                 }
             }
