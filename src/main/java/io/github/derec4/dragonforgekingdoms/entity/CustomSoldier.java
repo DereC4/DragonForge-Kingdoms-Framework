b7d5a696d0b535f7e2e3c3520a8195debae54e04
Derek Chen <derex1987@gmail.com>
2025-03-24 00:51:26 -0500

    feat: setup soldier and generalize target non faction players to accept other entities

--- src/main/java/io/github/derec4/dragonforgekingdoms/entity/CustomSoldier.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/entity/CustomSoldier.java
@@ -1,12 +1,11 @@
 package io.github.derec4.dragonforgekingdoms.entity;
 
-
 import lombok.Getter;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.monster.WitherSkeleton;
+import net.minecraft.world.entity.monster.Skeleton;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantments;
@@ -15,12 +14,12 @@ import net.minecraft.world.level.Level;
 import java.util.UUID;
 
 @Getter
-public class CustomWitherSkeleton extends WitherSkeleton {
+public class CustomSoldier extends Skeleton {
 
     private final UUID kingdomID;
 
-    public CustomWitherSkeleton(Level world, UUID kingdomID) {
-        super(EntityType.WITHER_SKELETON, world);
+    public CustomSoldier(Level world, UUID kingdomID) {
+        super(EntityType.SKELETON, world);
         this.kingdomID = kingdomID;
         this.collides = true;
         this.expToDrop = 0;
@@ -32,6 +31,7 @@ public class CustomWitherSkeleton extends WitherSkeleton {
         this.setCustomNameVisible(true);
         this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.0D, true));
         this.targetSelector.addGoal(1, new TargetNonFactionPlayersGoal(this, kingdomID));
+
         this.setItemSlot(EquipmentSlot.CHEST, new ItemStack(Items.NETHERITE_CHESTPLATE));
         this.setItemSlot(EquipmentSlot.LEGS, new ItemStack(Items.NETHERITE_LEGGINGS));
         this.setItemSlot(EquipmentSlot.FEET, new ItemStack(Items.NETHERITE_BOOTS));
--- src/main/java/io/github/derec4/dragonforgekingdoms/entity/TargetNonFactionPlayersGoal.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/entity/TargetNonFactionPlayersGoal.java
@@ -1,32 +1,33 @@
 package io.github.derec4.dragonforgekingdoms.entity;
 
 import io.github.derec4.dragonforgekingdoms.kingdom.KingdomManager;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.goal.target.TargetGoal;
 import net.minecraft.world.entity.player.Player;
 import org.bukkit.event.entity.EntityTargetEvent;
 
 import java.util.UUID;
 
-public class TargetNonFactionPlayersGoal extends TargetGoal {
-    private final CustomWitherSkeleton customWitherSkeleton;
+public class TargetNonFactionPlayersGoal<T extends Mob> extends TargetGoal {
+    private final T customMob;
     private final UUID kingdomID;
     private final KingdomManager kingdomManager;
 
-    public TargetNonFactionPlayersGoal(CustomWitherSkeleton customWitherSkeleton, UUID kingdomID) {
-        super(customWitherSkeleton, false);
+    public TargetNonFactionPlayersGoal(T customMob, UUID kingdomID) {
+        super(customMob, false);
         System.out.println("TEMPTEMPTEMP " + kingdomID);
-        this.customWitherSkeleton = customWitherSkeleton;
+        this.customMob = customMob;
         this.kingdomID = kingdomID;
         this.kingdomManager = KingdomManager.getInstance();
     }
 
     @Override
     public boolean canUse() {
-        for (Player player : this.customWitherSkeleton.level().players()) {
+        for (Player player : this.customMob.level().players()) {
             UUID playerKingdomID = kingdomManager.getPlayerKingdom(player.getUUID()).getID();
             // CHANGE LATER TEMP TEMP TEMP
             if (!player.isCreative() && !player.isSpectator() && kingdomID.equals(playerKingdomID)) {
-                this.customWitherSkeleton.setTarget(player, EntityTargetEvent.TargetReason.CLOSEST_PLAYER, true);
+                this.customMob.setTarget(player, EntityTargetEvent.TargetReason.CLOSEST_PLAYER, true);
                 return true;
             }
         }
