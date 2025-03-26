2b4d472259abba5d25228ea0fec86a80ee0501f1
Derek Chen <derex1987@gmail.com>
2025-03-26 04:22:21 -0500

    chore: remove unused imports

--- src/main/java/io/github/derec4/dragonforgekingdoms/entity/CustomSpawnEgg.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/entity/CustomSpawnEgg.java
@@ -1,23 +1,11 @@
 package io.github.derec4.dragonforgekingdoms.entity;
 
-import io.github.derec4.dragonforgekingdoms.entity.CustomGuard;
-import io.github.derec4.dragonforgekingdoms.entity.CustomSoldier;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.level.Level;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-
-import java.util.Objects;
-import java.util.UUID;
 
 public class CustomSpawnEgg extends Item implements Listener {
 
