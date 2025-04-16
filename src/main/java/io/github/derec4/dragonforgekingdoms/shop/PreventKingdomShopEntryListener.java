e209c42e67a0ec7f6da53066a576e02cf9c65f5c
Derek Chen <derex1987@gmail.com>
2025-04-15 22:54:18 -0500

    feat: think i got the deny shop buy thing done

--- src/main/java/io/github/derec4/dragonforgekingdoms/shop/PreventKingdomShopEntryListener.java
+++ src/main/java/io/github/derec4/dragonforgekingdoms/shop/PreventKingdomShopEntryListener.java
@@ -2,8 +2,13 @@ package io.github.derec4.dragonforgekingdoms.shop;
 
 import io.github.derec4.dragonforgekingdoms.kingdom.Kingdom;
 import io.github.derec4.dragonforgekingdoms.kingdom.KingdomManager;
+import io.github.derec4.dragonforgekingdoms.util.PermissionLevel;
+import io.github.derec4.dragonforgekingdoms.util.PlayerUtils;
 import me.gypopo.economyshopgui.api.events.PostTransactionEvent;
+import me.gypopo.economyshopgui.api.events.PreTransactionEvent;
 import me.gypopo.economyshopgui.objects.ShopItem;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -15,25 +20,23 @@ public class PreventKingdomShopEntryListener implements Listener {
         this.kingdomManager = kingdomManager;
     }
 
+    /**
+     * Use PreTransactionEvent, check if the player is allowed to buy kingdom eggs (and any future items added
+     * under the section titled "kingdom" (case insensitive). If not a duke or lord in LuckPerms, deny the transaction
+     * @param event
+     */
     @EventHandler
-    public void onPostTransaction(PostTransactionEvent event) {
+    public void onPreTransaction (PreTransactionEvent event) {
         Player player = event.getPlayer();
-        ShopItem shopItem = event.getShopItem();
+        String sectionTitle = event.getShopItem().getSubSection();
 
-        if (shopItem.getSubSection().equalsIgnoreCase("kingdom")) {
-            Kingdom kingdom = kingdomManager.getPlayerKingdom(player.getUniqueId());
+        if (sectionTitle.equalsIgnoreCase("Kingdom")) {
+            PermissionLevel rank = PlayerUtils.getPlayerRank(player);
 
-            if (kingdom != null) {
-                double cost = event.getAmount();
-
-                if (kingdom.getWealth() >= cost) {
-                    kingdom.giveWealth((int) -cost);
-                    player.sendMessage("The cost of the spawn egg has been deducted from your kingdom's wealth.");
-                } else {
-                    player.sendMessage("Your kingdom does not have enough wealth to purchase this item.");
-                }
-            } else {
-                player.sendMessage("You are not part of a kingdom.");
+            if (rank != PermissionLevel.LORD && rank != PermissionLevel.DUKE) {
+                player.sendMessage(ChatColor.RED + "You must be a Lord or Duke to access the Kingdom shop section.");
+                Bukkit.getLogger().info("Player " + player.getUniqueId() + " was denied a purchase in Kingdom shop");
+                event.setCancelled(true);
             }
         }
     }
